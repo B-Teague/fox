e3dd5940567db4e445adffe43ddc28b9e76a4060
Brian Teague <br.teague@gmail.com>
2023-09-30 18:32:29 -0400

    Update folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
-mod parser;
+extern crate fox_lib;
+#[macro_use]
+extern crate clap;
+extern crate nom;
+
+use fox_lib::evaluator::*;
+use fox_lib::lexer::token::*;
+use fox_lib::lexer::*;
+use fox_lib::parser::*;
+use nom::Err;
+use std::fs::File;
+use std::io::prelude::*;
+
+use crate::cmd::*;
+mod cmd;
+
+fn read_file(file_path: String) -> Result<String, ::std::io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
 
 fn main() {
-    let input = "multiply(add(1,2),add(3,4))";
-    let result = parser::parse_expr(input);
+    let code_string = match cmd::read_command() {
+        Command::FileRead(file_path) => read_file(file_path).ok(),
+        Command::RunInlineCode(code) => Some(code),
+        Command::Noop => None,
+    };
 
-    match result {
-        Ok(expr) => {
-            println!("Parsed expression: {:?}", expr);
-            // You can now use the parsed expression for further processing
-        }
-        Err(err) => {
-            println!("Parsing error: {:?}", err);
+    if let Some(code_string) = code_string {
+        let mut evaluator = Evaluator::new();
+        let lex_tokens = Lexer::lex_tokens(&code_string);
+        match lex_tokens {
+            Ok((_, r)) => {
+                let tokens = Tokens::new(&r);
+                let parsed = Parser::parse_tokens(tokens);
+                match parsed {
+                    Ok((_, program)) => {
+                        let eval = evaluator.eval_program(program);
+                        println!("{}", eval);
+                    }
+                    Err(Err::Error(_)) => println!("Parser error"),
+                    Err(Err::Failure(_)) => println!("Parser failure"),
+                    Err(Err::Incomplete(_)) => println!("Incomplete parsing"),
+                }
+            }
+            Err(Err::Error(_)) => println!("Lexer error"),
+            Err(Err::Failure(_)) => println!("Lexer failure"),
+            Err(Err::Incomplete(_)) => println!("Incomplete lexing"),
         }
     }
 }
